8682fcce81f8e58bb6d19360b375f81e86eb574b
K Aditya <165629293+Aditya-K-369@users.noreply.github.com>
2025-08-19 09:55:36 +0530

    using recursion

---
+++ ReverseStringRecursion.java
@@ -0,0 +1,13 @@
+public class  ReverseStringRecursion{
+    public static void reverse(String s){
+        if(s.length() ==0){
+            return ;
+        }
+        String ros = s.substring(1);
+        reverse(ros);
+        System.out.print(s.charAt(0));
+    }
+    public static void main(String[] args){
+        reverse("binod");
+    }
+}
