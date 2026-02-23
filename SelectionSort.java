a028b7ba683623a1ff705782ac54a453c83e488f
K Aditya <165629293+Aditya-K-369@users.noreply.github.com>
2026-02-23 21:59:35 +0530

    Correct implementation of selection sort

--- SelectionSort.java
+++ SelectionSort.java
@@ -3,14 +3,15 @@ import java.util.Scanner;
 public class SelectionSort {
 
     static void Sl(int arr[]) {
-        for (int i = 0; i < 6; i++) {
+        int si = 0;
+        for (int i = 0; i < 5; i++) {
+            si = i;
             for (int j = i + 1; j < 6; j++) {
-                if (arr[i] > arr[j]) {
-                    int temp = arr[i];
-                    arr[i] = arr[j];
-                    arr[j] = temp;
+                if (arr[j] < arr[si]) {
+                    si = j;
                 }
             }
+            arr[si] = (arr[si] + arr[i]) - (arr[i] = arr[si]);
         }
     }
 
@@ -21,9 +22,10 @@ public class SelectionSort {
             arr[i] = in.nextInt();
         }
         Sl(arr);
-        for(int i=0;i<6;i++){
-            System.out.print(arr[i]+"-> ");
+        for (int i = 0; i < 6; i++) {
+            System.out.print(arr[i] + "-> ");
         }
+        in.close();
     }
 
 }
