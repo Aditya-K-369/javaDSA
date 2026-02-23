8abdc9d5bfbb6455d682273a42e68edbefcbbd08
K Aditya <165629293+Aditya-K-369@users.noreply.github.com>
2026-02-23 21:33:39 +0530

    Insertion Sort

---
+++ INSERTIONSORT.java
@@ -0,0 +1,28 @@
+import java.util.*;
+
+public class INSERTIONSORT {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int arr[] = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+        int curr = 0, prev = 0;
+        for (int i = 1; i < n; i++) {
+            curr = arr[i];
+            prev = i - 1;
+            // while (prev >= 0 && arr[prev] > curr) {
+            // arr[prev + 1] = arr[prev];
+            // prev--;
+            // }
+            for (; prev >= 0 && arr[prev] > curr; prev--) {
+                arr[prev + 1] = arr[prev];
+            }
+            arr[prev + 1] = curr;
+        }
+        for (int i = 0; i < n; i++) {
+            System.out.println(arr[i]);
+        }
+    }
+}
