7edfdf6fc18b5634148496e4b339d2370c79b6e0
K Aditya <165629293+Aditya-K-369@users.noreply.github.com>
2026-03-04 10:10:54 +0530

    DFS BFS GRAPH TRAVERSAL

---
+++ Graph.java
@@ -0,0 +1,70 @@
+import java.util.*;
+
+public class Graph {
+    int v;
+    ArrayList<ArrayList<Integer>> adj;
+
+    Graph(int v) {
+        this.v = v;
+        adj = new ArrayList<>();
+        for (int i = 0; i < v; i++) {
+            adj.add(new ArrayList<>());
+        }
+    }
+
+    public void addEdge(int u, int v) {
+        adj.get(u).add(v);
+        adj.get(v).add(u);
+    }
+
+    public void printAdj() {
+        for (int i = 0; i < v; i++) {
+            System.out.println(i + "->" + adj.get(i).toString());
+        }
+    }
+
+    public void bfs() {
+        Queue<Integer> que = new LinkedList<>();
+        que.add(0);
+        boolean[] vis = new boolean[v];
+        vis[0] = true;
+        while (que.size() > 0) {
+            int u = que.remove();
+            System.out.println(u);
+            for (int v : adj.get(u)) {
+                if (!vis[v]) {
+                    vis[v] = true;
+                    que.add(v);
+                }
+            }
+        }
+
+    }
+
+    void dfsHelper(int u, boolean[] vis) {
+        System.out.println(u);
+        vis[u] = true;
+        for (int v : adj.get(u)) {
+            if (!vis[v]) {
+                dfsHelper(v, vis);
+            }
+        }
+    }
+
+    void dfs() {
+        boolean[] vis = new boolean[v];
+        dfsHelper(0, vis);
+    }
+
+    public static void main(String[] args) {
+        Graph g = new Graph(5);
+        g.addEdge(0, 1);
+        g.addEdge(1, 2);
+        g.addEdge(1, 3);
+        // g.addEdge(2, 3);
+        g.addEdge(2, 4);
+        g.printAdj();
+        g.bfs();
+        // g.dfs();
+    }
+}
--- QuickSort.java
+++ QuickSort.java
@@ -1,12 +1,12 @@
 import java.util.Arrays;
 
 public class QuickSort {
-    public static int partition(int arr[],int st,int end){
-        int indx = st-1,j,pivot = arr[end];
-        for(j=st;j<end;j++){
-            if(arr[j]<=pivot){
+    public static int partition(int arr[], int st, int end) {
+        int indx = st - 1, j, pivot = arr[end];
+        for (j = st; j < end; j++) {
+            if (arr[j] <= pivot) {
                 indx++;
-                if (indx != j) { 
+                if (indx != j) {
                     arr[indx] = arr[indx] ^ arr[j];
                     arr[j] = arr[indx] ^ arr[j];
                     arr[indx] = arr[indx] ^ arr[j];
@@ -14,23 +14,25 @@ public class QuickSort {
             }
         }
         indx++;
-        if (indx != j) { 
+        if (indx != j) {
             arr[indx] = arr[indx] ^ arr[j];
             arr[j] = arr[indx] ^ arr[j];
             arr[indx] = arr[indx] ^ arr[j];
         }
         return indx;
     }
-    public static void sort(int arr[] ,int st,int end){
-        if(st<end){
-            int pivot = partition(arr,st,end);
-            sort(arr, st, pivot-1);
-            sort(arr, pivot+1, end);
+
+    public static void sort(int arr[], int st, int end) {
+        if (st < end) {
+            int pivot = partition(arr, st, end);
+            sort(arr, st, pivot - 1);
+            sort(arr, pivot + 1, end);
         }
     }
-    public static void main(String args[]){
-        int arr[] = {35,243,342,12,45,2,4};
-        sort(arr,0,arr.length-1);
-        System.out.println(Arrays.toString(arr));     
+
+    public static void main(String args[]) {
+        int arr[] = { 35, 243, 342, 12, 45, 2, 4 };
+        sort(arr, 0, arr.length - 1);
+        System.out.println(Arrays.toString(arr));
     }
 }
